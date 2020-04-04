e47262cae73cfb09ed627ce569479a3e063c5e13
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2020-04-04 14:41:19 +0700

    set variable-prod

--- config/variabel/variabel-prod.js
+++ config/variabel/variabel-prod.js
@@ -1,5 +1,4 @@
 module.exports = {
   SECRET_OR_KEY: process.env.SECRET_OR_KEY,
-  OWNER_EMAIL: process.env.OWNER_EMAIL,
-  OWNER_EMAIL_PASSWORD: process.env.OWNER_EMAIL_PASSWORD
+  mongoURI: process.env.MONGO_URI
 };
