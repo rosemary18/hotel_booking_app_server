077c09bcfc0b36a1c573442f19db36f85669d79d
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2020-04-11 16:07:09 +0700

    add roomRoutes

--- model/RoomsModel.js
+++ model/RoomsModel.js
@@ -5,31 +5,28 @@ const { Schema } = mongoose;
 const schema = new Schema({
   rm_type: {
     type: Schema.Types.ObjectId,
-    ref: "RoomsType"
-  },
-  rm_available: {
-    type: Boolean,
-    default: false
+    ref: "RoomsType",
   },
   rm_number: {
     type: Number,
-    default: 0
+    default: 0,
   },
   rm_max_person: {
     adult: {
       type: Number,
-      default: 0
+      default: 0,
     },
     children: {
       type: Number,
-      default: 0
-    }
+      default: 0,
+    },
   },
   rm_price: {
     type: Number,
-    default: 0
+    default: 0,
   },
-  rm_inventory: []
+  rm_inventory: [],
+  rm_booked_date: [],
 });
 
 module.exports = mongoose.model("Rooms", schema);
--- routes/api/RoomsRoutes.js
+++ routes/api/RoomsRoutes.js
@@ -1,39 +1,80 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require("passport");
 // Models
 const { Room, RoomType } = require("../../model");
+const middleware = passport.authenticate("jwt", { session: false });
 
 // Routes
 
 // @Get TEST
 router.get("/fill", (req, res) => {
-  const datafill = new Room({
+  const datafillStandart = new Room({
     rm_type: "5e8725718c3819314cd62cd0",
-    rm_available: true,
-    rm_number: 103,
+    rm_number: 401,
     rm_max_person: {
       adult: 2,
-      children: 3
+      children: 3,
     },
     rm_price: 50,
-    rm_inventory: ["WI-FI", "Hot Water"]
+    rm_inventory: ["WI-FI", "Hot Water", "BreakFast"],
+    rm_booked_date: [],
   });
-  datafill.save().then(result => {
+  const datafillDeluxe = new Room({
+    rm_type: "5e87258a67a0e123582278be",
+    rm_number: 403,
+    rm_max_person: {
+      adult: 2,
+      children: 3,
+    },
+    rm_price: 188,
+    rm_inventory: [
+      "WI-FI",
+      "Hot Water",
+      "BreakFast",
+      "Free Message",
+      "Free Dinner",
+    ],
+    rm_booked_date: [],
+  });
+  const datafillSuit = new Room({
+    rm_type: "5e8725e648bb8d371cfc721a",
+    rm_number: 404,
+    rm_max_person: {
+      adult: 2,
+      children: 3,
+    },
+    rm_price: 188,
+    rm_inventory: [
+      "WI-FI",
+      "Hot Water",
+      "WI-FI",
+      "Hot Water",
+      "BreakFast",
+      "Free Message",
+      "Free Dinner",
+    ],
+    rm_booked_date: [],
+  });
+  datafillSuit.save().then((result) => {
     res.send(result);
   });
 });
 
 router.get("/type", (req, res) => {
-  RoomType.find().then(result => {
+  RoomType.find().then((result) => {
     res.send(result);
   });
 });
 
-router.get("/", (req, res) => {
-  Room.find().then(result => {
-    res.send(result);
-  });
+router.get("/", middleware, async (req, res) => {
+  const dataRoom = await Room.find()
+    .populate({
+      path: "rm_type",
+      model: "RoomsType",
+    })
+    .select();
+  res.status(200).json(dataRoom);
 });
 
 module.exports = router;
